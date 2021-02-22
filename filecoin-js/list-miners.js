86e44406551094234d12b514d48e5ee55464bf1a
Jim Pick <jim@jimpick.com>
2021-02-21 17:22:00 -0800

    Add list miners example

--- filecoin-js/list-miners.js
+++ filecoin-js/list-miners.js
@@ -7,6 +7,6 @@ const server = ganache.server({
 server.listen(7777, async (err, blockchain) => {
   const conn = new HttpJsonRpcConnector({ url: 'http://127.0.0.1:7777/rpc/v0' })
   const client = new LotusClient(conn)
-  console.log(await client.common.version())
+  console.log(await client.state.listMiners())
   process.exit()
 })
