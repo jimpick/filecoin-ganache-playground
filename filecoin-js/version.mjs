f5c379e2274c1f7d9ef2eab65e3a4c2786b9dbb2
Jim Pick <jim@jimpick.com>
2021-02-21 16:28:35 -0800

    Top-level await!

--- filecoin-js/version.mjs
+++ filecoin-js/version.mjs
@@ -1,13 +1,5 @@
 import { HttpJsonRpcConnector, LotusClient } from 'filecoin.js'
 
-;(async () => {
-  const httpConnector = new HttpJsonRpcConnector({
-    url: 'http://127.0.0.1:7777/rpc/v0'
-  })
-
-  const jsonRpcProvider = new LotusClient(httpConnector)
-  const version = await jsonRpcProvider.common.version()
-  console.log(version)
-})()
-  .then()
-  .catch()
+const conn = new HttpJsonRpcConnector({ url: 'http://127.0.0.1:7777/rpc/v0' })
+const client = new LotusClient(conn)
+console.log(await client.common.version())
